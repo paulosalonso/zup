4280977704c4a2b5b4a8d03f15948b880edc7f1a
Paulo Alonso <paulo_alonso_@hotmail.com>
2020-08-15 16:50:51 -0300

    Remoção de importação desnecessária.

--- src/test/java/com/github/paulosalonso/zup/domain/model/ContactTest.java
+++ src/test/java/com/github/paulosalonso/zup/domain/model/ContactTest.java
@@ -2,7 +2,6 @@ package com.github.paulosalonso.zup.domain.model;
 
 import nl.jqno.equalsverifier.EqualsVerifier;
 import nl.jqno.equalsverifier.Warning;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
