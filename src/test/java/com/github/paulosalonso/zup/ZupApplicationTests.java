456e309ebc75c17927bf15520fff2818b4450dd2
Paulo Alonso <paulo_alonso_@hotmail.com>
2020-08-15 11:05:34 -0300

    Inicialização do projeto.

---
+++ src/test/java/com/github/paulosalonso/zup/ZupApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.paulosalonso.zup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZupApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
